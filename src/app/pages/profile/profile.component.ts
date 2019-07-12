69037760a84d4da95374bd3faa3ebb7b18c7e722
skylock <emanuil.copil@gmail.com>
2019-07-12 18:14:24 +0300

    Update mocked app user

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -12,16 +12,16 @@ export class ProfileComponent {
   constructor() {
     this.employee = {
       ID: 7,
-      FirstName: 'Sandra',
-      LastName: 'Johnson',
-      Prefix: 'Mrs.',
-      Position: 'Controller',
-      Picture: 'images/employees/06.png',
-      BirthDate: new Date('1974/11/15'),
-      HireDate: new Date('2005/05/11'),
+      FirstName: 'Emanuil',
+      LastName: 'Copil',
+      Prefix: 'Mr.',
+      Position: 'Student',
+      Picture: 'https://randomuser.me/api/portraits/men/79.jpg',
+      BirthDate: new Date('1980/11/15'),
+      HireDate: new Date('2019/07/15'),
       /* tslint:disable-next-line:max-line-length */
-      Notes: 'Sandra is a CPA and has been our controller since 2008. She loves to interact with staff so if you`ve not met her, be certain to say hi.\r\n\r\nSandra has 2 daughters both of whom are accomplished gymnasts.',
-      Address: '4600 N Virginia Rd.'
+      Notes: '',
+      Address: 'Bulevardul Muncii, nr. 103-105, 400641, Cluj-Napoca, România'
     };
   }
 }
