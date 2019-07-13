d0f2da2e16db27702922b7c1505825f406300675
skylock <emanuil.copil@gmail.com>
2019-07-13 16:39:55 +0300

    Update home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,13 @@ import { AppInfoService } from '../../shared/services';
 })
 
 export class HomeComponent {
+  galleryDataSource = [{
+    name: 'Microscopul de forță atomică AFM-XE 70',
+    path: 'https://ww1.prweb.com/prfiles/2007/05/22/528401/PARKXE70AFM.jpg',
+    style: 'display: block; max-width:230px; max-height:95px;width: auto; height: auto;'
+  }];
+  selectedItem = this.galleryDataSource[0];
+
   constructor(public appInfo: AppInfoService) {
 
   }
